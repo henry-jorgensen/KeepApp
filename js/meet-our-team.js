7c061751da9a4eae604ec32b704f42454d93c5fe
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-03-17 16:16:43 +1030

    add tutorial page and directory update

--- js/meet-our-team.js
+++ js/meet-our-team.js
@@ -8,13 +8,12 @@ teamHeader.classList.remove('transition-team-header');
 const teamObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        
-        
         setTimeout(() =>{
             meetTeam.classList.add('meet-team-transition');
             teamImg.src = "assets/about/Brain-White.png"
             teamHeader.classList.add('transition-team-header');
             teamh1.classList.add('transition-h1');
+            teamh1.innerHTML = "Meet our Team"
         }, 1400);
         
         return;
@@ -23,6 +22,7 @@ const teamObserver = new IntersectionObserver(entries => {
       teamHeader.classList.remove('transition-team-header');
       teamh1.classList.remove('transition-h1');
       teamImg.src = "assets/about/Brain-Dark.png"
+      teamh1.innerHTML = "Brains at Work"
 
     });
   });
