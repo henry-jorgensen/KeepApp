698f4dabab6fa05fbd75c9b461ece6fca0fb2cad
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-03-24 10:40:00 +1030

    added buttons

--- js/client-reviews.js
+++ js/client-reviews.js
@@ -2,8 +2,8 @@ const content = document.querySelector('.customer-reviews-content');
 const trigger = document.querySelector('.client-animation-trigger');
 const phone = document.querySelector('.review-phone');
 const phoneText = document.querySelector('.phone-text');
-const reviews = document.querySelector('.customer-reviews')
-
+const reviews = document.querySelector('.customer-reviews');
+const buttons = document.getElementsByClassName('review-button');
 
 // Create the observer:
 const observer = new IntersectionObserver(entries => {
@@ -13,12 +13,18 @@ const observer = new IntersectionObserver(entries => {
       phone.classList.add('review-phone-transform');
       phoneText.classList.add('hide');
       reviews.classList.remove('hide');
+      for (var i = 0; i < buttons.length; i++){
+          buttons[i].classList.remove('hide')
+      }
       return;
     }
     content.classList.remove('customer-reviews-background-change')
     phone.classList.remove('review-phone-transform');
     phoneText.classList.remove('hide');
     reviews.classList.add('hide');
+    for (var i = 0; i < buttons.length; i++){
+        buttons[i].classList.add('hide')
+    }
   });
 });
 
