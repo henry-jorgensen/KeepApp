819ca8470b3421ae53bd3a74fe44e33397688d51
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-04-07 15:53:06 +0930

    scrolling horizontally on features section

--- js/slideshow-feature.js
+++ js/slideshow-feature.js
@@ -1,24 +1,24 @@
-var slideIndex = 1;
-showSlidesFeature(slideIndex);
+// var slideIndex = 1;
+// showSlidesFeature(slideIndex);
 
-function plusSlidesFeature(n) {
-  showSlidesFeature(slideIndex += n);
-}
+// function plusSlidesFeature(n) {
+//   showSlidesFeature(slideIndex += n);
+// }
 
-function currentSlideFeature(n) {
-  showSlidesFeature(slideIndex = n);
-}
+// function currentSlideFeature(n) {
+//   showSlidesFeature(slideIndex = n);
+// }
 
-function showSlidesFeature(n) {
-  var i;
-  var slides = document.getElementsByClassName("feature");
+// function showSlidesFeature(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("feature");
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
 
-  slides[slideIndex-1].style.display = "inline-flex";  
+//   slides[slideIndex-1].style.display = "inline-flex";  
 
-}
+// }
