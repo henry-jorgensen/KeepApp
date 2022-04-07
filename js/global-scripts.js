14748cd88e6a1a91535777cbac8272da92cd589f
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-04-07 10:57:29 +0930

    little changes to responsiveness accross board

--- js/global-scripts.js
+++ js/global-scripts.js
@@ -6,6 +6,7 @@ var navbar = document.getElementById("nav");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
+console.log("STICKY VALUE" + sticky)
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
