215818afce7b260f7423548e09b4302fd42baaee
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-04-08 13:36:47 +0930

    rudimentary top bar responsiveness + others

--- js/global-scripts.js
+++ js/global-scripts.js
@@ -6,7 +6,24 @@ var navbar = document.getElementById("nav");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-console.log("STICKY VALUE" + sticky)
+
+window.addEventListener('resize', windowResize);
+
+function windowResize(){
+  sticky = navbar.offsetTop
+  console.log(sticky)
+
+
+}
+
+// navbar.addEventListener('resize', navResize);
+
+// function navResize(){
+//   console.log("test")
+//   var theHeight = navbar.offsetHeight;
+//   console.log(theHeight)
+// }
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
