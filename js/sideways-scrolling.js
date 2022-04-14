ec0e93e47a6522525500f698c90f89a43abaa744
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-04-14 12:19:15 +0930

    rudimentary conditionfor scrolling at end or start

--- js/sideways-scrolling.js
+++ js/sideways-scrolling.js
@@ -1,13 +1,53 @@
 $(function() {
 
-    $(".slideshow").mousewheel(function(event, delta) {
-       const element = document.getElementById("test"); 
+    $(".horizontal-scroll").mousewheel(function(event, delta) {
+       const element = document.querySelector(".what-we-do"); 
+
+       const elements = document.getElementsByClassName("what-we-do");
+       const elementsIndexLength = elements.length - 1 
+
+       elements[0].id = "Start";
+       elements[elementsIndexLength].id="Final";
+       
+       const featureElements = document.getElementsByClassName("feature")
+       const featureElementsIndexLength = featureElements.length -1
+
+       featureElements[0].id = "Start";
+       featureElements[featureElementsIndexLength].id="Final";
+
+       
        this.scrollLeft -= (delta * element.clientWidth);
        
-       console.log(element.clientWidth);
-       console.log(delta);
-       console.log();
-       event.preventDefault();
+       var end = false;
+       var start = false;
+       if(delta == -1){
+           start = false
+           console.log("RIGHT");
+           
+           if(event.originalEvent.path[2].id == "Final"){
+               console.log("YEYEYEYEYEYE")
+               end = true;
+           }
+           console.log("START :" + start);
+           console.log("END: " + end);
+       } else{
+           end = false;
+           console.log("LEFT");
+           
+           if(event.originalEvent.path[2].id == "Start"){
+                start = true;
+           }
+           console.log("START :" + start);
+           console.log("END: " + end);
+
+       }
+
+       if (end == true || start == true){
+        console.log("At the end")
+       } else{
+        event.preventDefault();
+       }
+       
  
     });
  
