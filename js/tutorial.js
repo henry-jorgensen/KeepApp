009e4a8277ec666d93174479ddf32b9e26bf6479
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-05-06 13:47:28 +0930

    refined tutorial responsiveness

--- js/tutorial.js
+++ js/tutorial.js
@@ -2,18 +2,26 @@ var expandButton = document.getElementById('expand');
 var leftMenuColumn = document.getElementById('columnMenu');
 var darkener = document.getElementById('darken')
 
+var small = false
 var temp = false
 const myObserver = new ResizeObserver(entries => {
     entries.forEach(entry => {
       var width = entry.contentRect.width;
       if(width >= 1096){
+          temp = false
           leftMenuColumn.style.opacity = "1"
           darkener.style.opacity = "0"
           expandButton.style.left = "1%"
           leftMenuColumn.style.pointerEvents = "all"
+          expandButton.style.transform = "rotateY(0deg)"
+          small = false
       } else{
-          leftMenuColumn.style.opacity = "0"
-          leftMenuColumn.style.pointerEvents = "none"
+          small = true
+          if(small == true && temp == false){
+            leftMenuColumn.style.opacity = "0"
+            leftMenuColumn.style.pointerEvents = "none"
+            temp = true;
+          }
       }
     });
 });
@@ -26,7 +34,12 @@ function showMenu(){
     if (opacityBool == false){
         leftMenuColumn.style.opacity = "1";
         leftMenuColumn.style.pointerEvents = "all"
-        expandButton.style.left = "70%";
+        if (small == true){
+            expandButton.style.left = "90%";
+        } else{
+            expandButton.style.left = "70%";
+        }
+        
         expandButton.style.transform = "rotateY(180deg)"
         darkener.style.opacity = "1"
         opacityBool = true
