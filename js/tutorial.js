42f71170a84c641bdd6705960c4e7a9fc3c34365
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-05-27 09:26:59 +0930

    testing issue on phone

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,5 +1,6 @@
 var expandButton = document.getElementById('expand');
 var leftMenuColumn = document.getElementById('columnMenu');
+var rightColumn = document.getElementById('rightCol')
 var darkener = document.getElementById('darken')
 
 var small = false
@@ -7,7 +8,7 @@ var temp = false
 const myObserver = new ResizeObserver(entries => {
     entries.forEach(entry => {
       var width = entry.contentRect.width;
-      if(width >= 1096){
+      if(width > 1088){
           temp = false
           leftMenuColumn.style.opacity = "1"
           darkener.style.opacity = "0"
@@ -34,20 +35,22 @@ function showMenu(){
     if (opacityBool == false){
         leftMenuColumn.style.opacity = "1";
         leftMenuColumn.style.pointerEvents = "all"
+        rightColumn.style.pointerEvents = "none"
         if (small == true){
             expandButton.style.left = "80%";
         } else{
             expandButton.style.left = "70%";
         }
         
-        expandButton.style.transform = "rotateY(180deg)"
+        //expandButton.style.transform = "rotateY(180deg)"
         darkener.style.opacity = "1"
         opacityBool = true
     } else{
         leftMenuColumn.style.opacity = "0";
         leftMenuColumn.style.pointerEvents = "none"
+        rightColumn.style.pointerEvents = "all"
         expandButton.style.left = "1%"
-        expandButton.style.transform = "rotateY(0deg)"
+        //expandButton.style.transform = "rotateY(0deg)"
         darkener.style.opacity = "0"
         opacityBool = false
     }
