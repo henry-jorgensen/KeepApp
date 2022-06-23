c8081d17fcad7164597708e7a17c36793ef2b12f
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-06-23 15:57:38 +0930

    mostly done video controls

--- js/video-controls.js
+++ js/video-controls.js
@@ -1,4 +1,4 @@
-var video = document.getElementById('video');
+const video = document.getElementById('video');
 
 const playButton = document.getElementById('play')
 const playButtonImg = document.getElementById('play-pause')
@@ -8,9 +8,14 @@ const duration = document.getElementById('duration')
 
 const progressBar = document.getElementById('progress-bar')
 const seek = document.getElementById('seek')
+const seekTooltip = document.getElementById('seek-tooltip')
 
 const fullscreenButton = document.getElementById('fullscreen-btn')
 const videoContainer = document.getElementById('video-container')
+
+const titleContainer = document.getElementById('title-container')
+const controlsContainer = document.getElementById('controls-container')
+const controls = document.getElementById('controls')
 
 function formatTime(timeInSeconds){
     const timeInMilliseconds = timeInSeconds * 1000
@@ -35,6 +40,7 @@ function updateTimeElapsed(){
 
 function updateProgress(){
     seek.value = Math.floor(video.currentTime)
+    progressBar.value = Math.floor(video.currentTime)
 }
 
 
@@ -42,10 +48,15 @@ function updateProgress(){
 function toggleFullScreen(){
     if (document.fullscreenElement){
         document.exitFullscreen()
+        titleContainer.style.top = "20px"
+        controlsContainer.style.bottom = "46px"
+        controls.style.borderRadius = "0px 0px 14px 14px"
     }
     else {
         videoContainer.requestFullscreen()
-        
+        titleContainer.style.top = "0"
+        controlsContainer.style.bottom = "67px"
+        controls.style.borderRadius = "0px"
     }
 }
 
@@ -56,8 +67,8 @@ function init() {
         
         const videoDuration = Math.round(video.duration)
         seek.setAttribute('max', videoDuration)
+        progressBar.setAttribute('max', videoDuration)
         const time = formatTime(videoDuration)
-        console.log(`Init ${time.milliseconds}`)
         duration.innerText = `${time.minutes}:${time.seconds}`;
         duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}`)
     }
@@ -65,17 +76,63 @@ function init() {
 
 document.addEventListener("DOMContentLoaded", init, false)
 
+function createIcon(type){
+    var typeRoute;
+    if (type == "play"){
+        console.log("HIT")
+        typeRoute = "./assets/tutorials/Play.png"
+    } else if (type == "pause"){
+        typeRoute = "./assets/tutorials/Pause.png"
+    } else {
+        return
+    }
+    var centreIcon = document.createElement('div')
+    centreIcon.classList.add('centre-icon')
+    var iconImg = document.createElement('img')
+    iconImg.src = typeRoute
+    centreIcon.append(iconImg)
+    videoContainer.append(centreIcon)
+    setTimeout(() => {
+        centreIcon.classList.add('fade-out')
+    }, 200);
+    setTimeout(() => {
+        centreIcon.remove()
+    }, 1000);
+
+}
+
 function play(event){
     if (video.paused || video.ended){
         playButtonImg.src = "assets/tutorials/Pause.png"
         video.play()
+        createIcon("play")
     } else {
         playButtonImg.src = "assets/tutorials/Play.png"
         video.pause()
+        createIcon("pause")
     }
 }
-
+video.addEventListener('click', play)
 video.addEventListener('timeupdate', updateTimeElapsed)
 video.addEventListener('timeupdate', updateProgress)
 
+function updateSeekToolTip(event){
+    const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
+    seek.setAttribute('data-seek', skipTo)
+    const t = formatTime(skipTo);
+    seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+    const rect = video.getBoundingClientRect();
+    seekTooltip.style.left = `${event.pageX - rect.left}px`;
+}
+
+seek.addEventListener('mousemove', updateSeekToolTip);
+
+function skipAhead(event){
+    const skipTo = event.target.dataset.seek ? event.target.dataset.seek : event.target.value;
+    video.currentTime = skipTo
+    progressBar.value = skipTo
+    seek.value = skipTo
+}
+
+seek.addEventListener('input', skipAhead)
 
