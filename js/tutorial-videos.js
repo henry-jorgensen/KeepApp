5df5469cafe1d8a19c2cc167f4f4b7e26e3f7b79
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-06-28 12:46:01 +0930

    little preparation changes

--- js/tutorial-videos.js
+++ js/tutorial-videos.js
@@ -1,25 +1,26 @@
-
 const sectionsData = [{id: 0, title: "What is Track my Kid?", completion: true }, 
                       {id: 1, title: "Download the app", completion: true}, 
                       {id: 2, title: "Register", completion: false}, 
                       {id: 3, title: "Lorem Ipsum", completion: false}, 
                       {id: 4, title: "Lorem Ipsum", completion: false}]
+                      
 
+const videoLinks = [{id: 0, route: "https://st.depositphotos.com/8469332/53417/v/600/depositphotos_534179478-stock-video-generic-php-source-code-building.mp4"}, 
+                    {id: 1, route: "./assets/videos/DroneTest.mp4"}, 
+                    {id: 2, route: "https://vod-progressive.akamaized.net/exp=1656393357~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1529%2F14%2F357646800%2F1460823501.mp4~hmac=7744db304d734e705b02ef95abb987a33cb9ef078211f7c56adc95c0e71123c3/vimeo-prod-skyfire-std-us/01/1529/14/357646800/1460823501.mp4?filename=video.mp4"}, 
+                    {id: 3, route: "https://vod-progressive.akamaized.net/exp=1656396686~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1588%2F14%2F357941044%2F1462749183.mp4~hmac=be40ee6356901a05b3a6f270aa16517fc59d9f8b11c3ff3235a158123dc75aee/vimeo-prod-skyfire-std-us/01/1588/14/357941044/1462749183.mp4?filename=video.mp4"}, 
+                    {id: 4, route: "https://vod-progressive.akamaized.net/exp=1656396732~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4611%2F14%2F373057302%2F1551388846.mp4~hmac=abbbb3447a05399a19ed6ea62982acea3ebe184a88398cb9e4429d1779543efd/vimeo-prod-skyfire-std-us/01/4611/14/373057302/1551388846.mp4?filename=video.mp4"}]
 
-const videoLinks = [{id: 0, video: "https://st.depositphotos.com/8469332/53417/v/600/depositphotos_534179478-stock-video-generic-php-source-code-building.mp4"}, 
-                    {id: 1, video: "./assets/videos/DroneTest.mp4"}, 
-                    {id: 2, video: "https://vod-progressive.akamaized.net/exp=1656393357~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1529%2F14%2F357646800%2F1460823501.mp4~hmac=7744db304d734e705b02ef95abb987a33cb9ef078211f7c56adc95c0e71123c3/vimeo-prod-skyfire-std-us/01/1529/14/357646800/1460823501.mp4?filename=video.mp4"}, 
-                    {id: 3, video: "https://vod-progressive.akamaized.net/exp=1656396686~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1588%2F14%2F357941044%2F1462749183.mp4~hmac=be40ee6356901a05b3a6f270aa16517fc59d9f8b11c3ff3235a158123dc75aee/vimeo-prod-skyfire-std-us/01/1588/14/357941044/1462749183.mp4?filename=video.mp4"}, 
-                    {id: 4, video: "https://vod-progressive.akamaized.net/exp=1656396732~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4611%2F14%2F373057302%2F1551388846.mp4~hmac=abbbb3447a05399a19ed6ea62982acea3ebe184a88398cb9e4429d1779543efd/vimeo-prod-skyfire-std-us/01/4611/14/373057302/1551388846.mp4?filename=video.mp4"}]
-
-const videoDescriptions = [{id: 0, title: "test", description: "h12"},
-                           {id: 1, title: "test", description: "h12"},
-                           {id: 2, title: "test", description: "h12"},
-                           {id: 3, title: "test", description: "h12"},
-                           {id: 4, title: "test", description: "h12"}]
+const videoDescriptions = [{id: 0, title: "What is Track my Kid", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed fringilla urna. Fusce rutrum nibh in dapibus bibendum.  Pellentesque accumsan egestas aliquam. Aliquam laoreet mauris ut libero sodales sodales. Ut vitae magna dolor. Proin consequat, arcu non vehicula ornare, massa erat cursus nisl, vulputate hendrerit sapien metus quis augue. Maecenas a diam porttitor ipsum dictum faucibus quis a dolor. Nulla lacinia tempus mauris, vel rhoncus ante pretium id. Integer non interdum nunc. Vivamus purus tortor, sollicitudin dapibus luctus placerat, venenatis eu mi. Integer urna ligula, varius id auctor eget, sollicitudin vitae enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam felis elit, congue elementum lorem et, facilisis mattis magna. Donec quis porttitor ante.\nNullam ullamcorper eu leo et suscipit. Duis dictum, nunc id pellentesque aliquet, nulla odio tincidunt risus, sed congue eros magna eget orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae dui eu libero sodales condimentum sed eget odio. Ut eu pretium lorem. Praesent ac volutpat mauris, id elementum diam. Aenean ac sollicitudin elit, at accumsan quam. In hac habitasse platea dictumst. Cras facilisis facilisis quam sit amet molestie. In tincidunt sagittis lacus, at cursus sem placerat sit amet. Praesent ut ligula eget odio sagittis dapibus nec nec lorem. Integer purus ante, rutrum sed tortor sit amet, semper mollis nulla. Etiam a est nec nibh molestie imperdiet a non mauris. Suspendisse eget sodales odio. Quisque nec commodo dui.\n Suspendisse aliquam ex nec est auctor ullamcorper. Nunc eget laoreet ipsum. Suspendisse consequat sed enim ultrices interdum. Sed in mi vitae velit iaculis rutrum a et leo. Nam quis semper neque, ac porta turpis. Nunc ornare ipsum tempus velit condimentum sollicitudin. Duis elementum consectetur dui. Maecenas ornare lacus at tempor dignissim. Praesent porttitor est vitae sapien convallis ornare. Etiam maximus dapibus ex, nec pellentesque lacus scelerisque vel. Nunc semper, elit et finibus dictum, diam ex dapibus lorem, a eleifend ex ligula lacinia tellus."},
+                           {id: 1, title: "Download the app", description: "h12"},
+                           {id: 2, title: "Register", description: "h12"},
+                           {id: 3, title: "Lorem Ipsum", description: "h12"},
+                           {id: 4, title: "Lorem Ipsum", description: "h12"}]
 
 const sectionNodes = document.getElementsByClassName('section')
 const videoHoverTitle = document.getElementById('video-hover-title')
+
+const videoTextContainer = document.getElementById('video-description-container')
 
 function fillSectionsLoop(){
     for (var i = 0; i < sectionsData.length; i++){
@@ -29,6 +30,7 @@ function fillSectionsLoop(){
         sectionNodes[i].addEventListener('click', clickSection)
     }
     videoHoverTitle.textContent = sectionsData[0].title
+    video.src = videoLinks[0].route
 }
 
 const sections = document.getElementById('sections')
@@ -68,7 +70,7 @@ function createSection(id, title, done){
 
 function updateVideo(id){
     videoHoverTitle.textContent = sectionsData[id].title
-    video.src = videoLinks[id].video
+    video.src = videoLinks[id].route
 }
 
 function clickSection(event){   
